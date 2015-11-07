5a2f6dcd147d3b214361abbde167a28c50e7bfd4
Steven Fuchs <stwf@mac.com>
2015-11-07 09:45:32 -0500

    bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule RandomAd.Mixfile do
 
   def project do
     [app: :random_ad,
-     version: "0.1.1",
+     version: "0.1.2",
      elixir: "~> 1.0",
      elixirc_paths: elixirc_paths(Mix.env),
      deps: deps,
