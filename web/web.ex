7408377945824d86c19f1f138564dabf2ed456f5
Steven Fuchs <stwf@mac.com>
2016-08-31 22:38:11 -0400

    ??

--- web/web.ex
+++ web/web.ex
@@ -11,8 +11,8 @@ defmodule RandomAd.Web do
       |> hd
       |> Path.join("templates")
 
-      use Phoenix.View, root: file_path
-      # use Phoenix.View, root: "web/templates"
+      #use Phoenix.View, root: file_path
+      use Phoenix.View, root: "web/templates"
 
       # Import convenience functions from controllers
       import Phoenix.Controller, only: [view_module: 1]
