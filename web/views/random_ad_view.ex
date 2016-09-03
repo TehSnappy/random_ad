86048a52cb381e61a9337cbd485e29ac8cc6bd80
Steven Fuchs <stwf@mac.com>
2016-09-03 17:32:25 -0400

    use set params

--- web/views/random_ad_view.ex
+++ web/views/random_ad_view.ex
@@ -3,14 +3,18 @@ defmodule RandomAd.RandomAdView do
 
 
 
-  def render(path) do
-    :random.seed(:erlang.now)
-    case File.ls(path) do
-      {:ok, pages} ->
-        shown_page = Enum.take_random(pages, 1)
-        raw(File.read!("#{path}/#{shown_page}"))
-      _ ->
-        "images not found at #{path}"
+  def render_random do
+    path = Application.get_env(:random_ad, :ad_path)
+    if (path) do
+      :random.seed(:erlang.now)
+      case File.ls(path) do
+        {:ok, pages} ->
+          shown_page = Enum.take_random(pages, 1)
+          raw(File.read!("#{path}/#{shown_page}"))
+        _ ->
+          "images not found at #{path}"
+      end
+    else
     end
   end
 
