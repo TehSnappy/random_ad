75dfabfb49de973d3f6dff102e74f9e3d7c63e48
Steven Fuchs <stwf@mac.com>
2016-09-08 12:40:28 -0400

    work better

--- web/views/random_ad_view.ex
+++ web/views/random_ad_view.ex
@@ -4,18 +4,18 @@ defmodule RandomAd.RandomAdView do
 
 
   def render_random do
-    path = Application.get_env(:random_ad, :ad_path)
-    if (not is_nil(path)) do
-      :random.seed(:os.timestamp)
-      case File.ls(path) do
-        {:ok, pages} ->
-          shown_page = Enum.take_random(pages, 1)
-          raw(File.read!("#{path}/#{shown_page}"))
-        _ ->
-          "images not found at #{path}"
-      end
-    else
-      "path not set"
+    case Application.get_env(:random_ad, :ad_path) do
+      {:ok, path} ->
+        :random.seed(:os.timestamp)
+        case File.ls(path) do
+          {:ok, pages} ->
+            shown_page = Enum.take_random(pages, 1)
+            raw(File.read!("#{path}/#{shown_page}"))
+          reason ->
+            "images not found at #{path}, #{reason}"
+        end
+      _ ->     
+        "path not set"
     end
   end
 
